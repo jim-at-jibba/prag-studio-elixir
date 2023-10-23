f8533a28a55d266ff039910645c77ff671f48e24
James Best <jim@justjibba.net>
2023-10-23 20:18:20 +0100

    Section 4 - pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -8,7 +8,17 @@ defmodule Servy.Handler do
 
   def parse(request) do
     # TODO: Parse the request string into a map:
-    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+    # key/value pair
+    # key is an atom - a constant whos name is its value
+    # first_line = request |> String.split("\n") |> List.first()
+    # [method, path, _] = String.split(first_line, " ")
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
