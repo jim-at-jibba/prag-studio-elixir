951e520ce16d01f9e4209e639440e159e287d630
James Best <jim@justjibba.net>
2023-10-25 14:05:47 +0100

    Section 8 - Pattern matching on maps

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,10 +2,34 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts("Warning: #{path} is on the loose")
+    conv
+  end
+
+  def track(conv), do: conv
+
+  # when pattern matching maps, you dont need to match all values
+  # but the left handside map must not contain anything the right
+  # hand side does not
+  # conv = %{method: "GET", path: "/wildlife"}
+  # %{path: "/wildlife"} = conv (MATCH)
+  # %{name: "nope", path: "/wildlife"} = conv (NOT MATCH)
+  # %{method: method, path: "/wildlife"} = conv (bind method to method from the conv)
+  # This will only be called if the path pattern matches, we are rewriting the path from
+  # /wildlife to wildthings
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "wildthings"}
+  end
+
+  def rewrite_path(conv), do: conv
 
   # def log(conv) do
   #   # IO.inspect prints and returns value
@@ -29,42 +53,61 @@ defmodule Servy.Handler do
     %{method: method, path: path, resp_body: "", status: nil}
   end
 
-  def route(conv) do
-    # conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  # def route(conv) do
+  #   # conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  #
+  #   # All data is immutable. When transforming a new version is returned.
+  #   # Its is common to just reassign the existing variable with the updated values
+  #
+  #   # This is very common so there is a shorthand why to do it. Only works on existing properties
+  #   # conv = Map.put(conv, "resp_body", "Bears, Lions, Tigers")
+  #   # %{conv | resp_body: "Bears, Lions, Tigers"}
+  #
+  #   # if conv.path == "/wildthings" do
+  #   #   %{conv | resp_body: "Bears, Lions, Tigers"}
+  #   # else
+  #   #   %{conv | resp_body: "Teddy, smoky, paddington"}
+  #   # end
+  #   route(conv, conv.method, conv.path)
+  # end
+  #
+  # # This is a function clause.
+  # # When functions has multiple clauses elixir will
+  # # try each one until it has a match
+  # def route(conv, "GET", "/wildthings") do
+  #   %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  # end
+  #
+  # def route(conv, "GET", "/bears") do
+  #   %{conv | status: 200, resp_body: "Teddy, smoky, paddington"}
+  # end
+  #
+  # # The third argument path, will be pattern matched against the incoming path
+  # # So /bears/1, the 1 is pattern matched to the id property
+  # def route(conv, "GET", "/bears/" <> id) do
+  #   %{conv | status: 200, resp_body: "Bear #{id}"}
+  # end
+  #
+  # def route(conv, _method, path) do
+  #   %{conv | status: 404, resp_body: "No #{path} here!"}
+  # end
 
-    # All data is immutable. When transforming a new version is returned.
-    # Its is common to just reassign the existing variable with the updated values
-
-    # This is very common so there is a shorthand why to do it. Only works on existing properties
-    # conv = Map.put(conv, "resp_body", "Bears, Lions, Tigers")
-    # %{conv | resp_body: "Bears, Lions, Tigers"}
-
-    # if conv.path == "/wildthings" do
-    #   %{conv | resp_body: "Bears, Lions, Tigers"}
-    # else
-    #   %{conv | resp_body: "Teddy, smoky, paddington"}
-    # end
-    route(conv, conv.method, conv.path)
-  end
-
-  # This is a function clause.
-  # When functions has multiple clauses elixir will
-  # try each one until it has a match
-  def route(conv, "GET", "/wildthings") do
+  # This above can be rewritten to use pattern matching on maps
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{method: "GET", path: "/bears"} = conv) do
     %{conv | status: 200, resp_body: "Teddy, smoky, paddington"}
   end
 
   # The third argument path, will be pattern matched against the incoming path
   # So /bears/1, the 1 is pattern matched to the id property
-  def route(conv, "GET", "/bears/" <> id) do
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(conv, _method, path) do
+  def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
@@ -104,6 +147,17 @@ Bears, Lions, Tigers
 # blank line is needed
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
